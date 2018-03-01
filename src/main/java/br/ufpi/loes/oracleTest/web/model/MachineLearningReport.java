e48e6ef2e4cd3994be1741acd9039221e4cf1f52
Jose Reis R Santiago <jsribeirosantiago4@gmail.com>
2018-03-01 20:19:37 -0300

    Remoção de uma biblioteca importada que não estava sendo usada.

--- src/main/java/br/ufpi/loes/oracleTest/web/model/MachineLearningReport.java
+++ src/main/java/br/ufpi/loes/oracleTest/web/model/MachineLearningReport.java
@@ -2,7 +2,6 @@ package br.ufpi.loes.oracleTest.web.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.persistence.CascadeType;
