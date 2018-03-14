b32fe10e55ab88dcba5802b2de91178baff0f20e
José Reis R Santiago <jsribeirosantiago4@gmail.com>
2018-03-14 16:54:59 -0300

    Correção da função de busca de dados estastitico e implementação da função de exibição do grafico.

--- src/main/webapp/resources/js/controllers/StatisticController.js
+++ src/main/webapp/resources/js/controllers/StatisticController.js
@@ -1,28 +1,70 @@
 angular.module("oracle-test").controller('StatisticController', [ '$scope', '$cookieStore', '$state', '$stateParams', '$filter', 'StatisticService', function($scope, $cookieStore, $state, $stateParams, $filter, StatisticService) {
+	var totalActions = 0;
 	
-	$scope.statistic = function() {
-		console.log($stateParams.id)
+	$scope.getTotalActions = function() {
+		$scope.totalActions = totalActions;
+	}
+	
+	$scope.statisticAction = function() {
 		StatisticService.getStatistics($stateParams.id).success(function(data) {
-			if(data.length != undefined) {
-//				$scope.statistic = data;
-				console.log(data);
-				
-				Morris.Area({
-					element: 'statistcs-actions',
-					data: data,
-					xkey: 'saveDate',
-					ykeys: 'actionsNumber',
-					pointSize: 2,
-					hideHover: 'auto',
-			        resize: true
-				});
-			} else {
-				$scope.error = "Não foi encontrada nenhuma estatisca para a aplicacao !!!"
+			var number = [], dates = [];
+			for(var i = 0; i < data.length; i++) {
+				number[i] = data[i].actionsNumber;
+				dates[i] = $filter('date')((data[i].saveDate), "dd/MM/yyyy");
 			}
+			
+			console.log(totalActions);
+			var ctx = document.getElementById('canvas').getContext('2d');
+			var config = {
+				type: 'line',
+				data: {
+					labels: dates,
+					datasets: [{
+						label: 'Actions',
+						backgroundColor: "#c05",
+						borderColor: "#c05",
+						data: number,
+						fill: false
+					}]
+				},
+				options: {
+					responsive: true,
+					title: {
+						display: true,
+						text: 'Acoes Capturadas'
+					},
+					tooltips: {
+						mode: 'index',
+						intersect: false,
+					},
+					hover: {
+						mode: 'nearest',
+						intersect: true
+					},
+					scales: {
+						xAxes: [{
+							display: true,
+							scaleLabel: {
+								display: true,
+								labelString: 'Dia'
+							}
+						}],
+						yAxes: [{
+							display: true,
+							scaleLabel: {
+								display: true,
+								labelString: 'Numero Acoes'
+							}
+						}]
+					}
+				}
+			}
+			
+			window.myLine = new Chart(ctx, config);
+			
 		}).error(function(data, status) {
 			//TODO: ADD MENSAGEM DE ERRO
 			console.log("Deu errado ao buscar as estastisticas da applicação !!!");
-		});
-	}
-	
+		});	
+	};
 }]);
