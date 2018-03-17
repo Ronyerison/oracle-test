8c9314c49eb567d62a73aefe1a8ea389217bc62d
Jose Reis R Santiago <jsribeirosantiago4@gmail.com>
2018-03-17 16:01:50 -0300

    Criação da funções que são responsaveis por exibir os dados de um relatório em gráficos. Foram criados função para exiição do percentual correto e incorreto em grafico de pizza (pie); uma função para exibição dos indices Kappa e Erro Rate em um grafico semelhante a um grafico pizza (modificado). Alem disso no controler StatisticController foi implementado um função para conversão de uma matriz, representada com uma string, em uma matriz de dados.

--- src/main/webapp/resources/js/controllers/StatisticController.js
+++ src/main/webapp/resources/js/controllers/StatisticController.js
@@ -1,14 +1,29 @@
 angular.module("oracle-test").controller('StatisticController', [ '$scope', '$cookieStore', '$state', '$stateParams', '$filter', 'StatisticService', function($scope, $cookieStore, $state, $stateParams, $filter, StatisticService) {
 	
-	// retorna a lista de simulações de uma aplicação
-	function getSimulations(idApp) {
+	function stringToMatriz(stringM) {
+		stringM = stringM.split("], ");
 		
+		// removendo todos caracteres '[', ']' e ' ', e tranformando em uma matriz de string
+		for(s in stringM) {
+			while(stringM[s].indexOf("[") != -1) {
+				stringM[s] = stringM[s].replace("[", "");
+			}
+			
+			if(s == stringM.length-1) {
+				while(stringM[s].indexOf("]") != -1) {
+					stringM[s] = stringM[s].replace("]", "");
+				}
+			}
+			
+			stringM[s] = stringM[s].split(",");
+		}
+		
+		return stringM;
 	}
 	
 	$scope.getTotalActions = function() {
 		StatisticService.getStatisticApplication($stateParams.id).success(function(data) {
 			var numberActions = 0;
-			console.log("total actions: ", data);
 			for(var i = 0; i < data.length; i++) {
 				numberActions = numberActions + data[i].actionsNumber;
 			}
@@ -22,7 +37,6 @@ angular.module("oracle-test").controller('StatisticController', [ '$scope', '$co
 	
 	$scope.statisticAction = function() {
 		StatisticService.getStatisticApplication($stateParams.id).success(function(data) {
-			console.log(data);
 			var number = [], dates = [];
 			for(var i = 0; i < data.length; i++) {
 				number[i] = data[i].actionsNumber;
@@ -75,13 +89,118 @@ angular.module("oracle-test").controller('StatisticController', [ '$scope', '$co
 				}
 			}
 			
-			window.myLine = new Chart(ctx, config);
+			window.statistics = new Chart(ctx, config);
 			
 		}).error(function(data, status) {
 			//TODO: ADD MENSAGEM DE ERRO
 			console.log("Deu errado ao buscar as estastisticas da applicação !!!");
 		});
 	};
+	
+	// Converte a string que representa a matriz
+	$scope.matrizConfusion = function() {
+		var matriz = [["V"], ["F"], ["PF"]];
+		var m = stringToMatriz($scope.simulation.report.confusionMatrixString);
+		
+		for(x in matriz) {
+			matriz[x] = matriz[x].concat(m[x]);
+		}
+		
+		$scope.simulation.report.confusionMatrixString = matriz;
+	}
+	
+	$scope.getSimulation = function() {
+		var simulation = StatisticService.getSimulation($stateParams.idSimulation);
+		
+		if(simulation !== undefined) {
+			console.log("DADOS DA SIMULACAO: ", simulation);
+			$scope.simulation = simulation;
+		} else {
+			console.log("ERRO: simulação não encontrada !!!");
+		}
+	}
+	
+	$scope.pieReportPct = function() {
+		var config = {
+			type: 'pie',
+			data: {
+				datasets: [{
+					data: [
+						$filter('number')($scope.simulation.report.pctCorrect),
+						$filter('number')($scope.simulation.report.pctIncorrect)
+					],
+					backgroundColor: [
+						window.chartColors.green,
+						window.chartColors.red
+					],
+					label: 'Dataset 1'
+				}],
+				labels: [
+					'Pct Correto (%): ',
+					'Pct Incorreto (%): '
+				]
+			},
+			options: {
+				responsive: true,
+				legend: {
+					display: false,
+					position: 'top',
+				},
+				title: {
+					display: true,
+					text: 'Percentual de Avaliacao'
+				},
+				animation: {
+					animateScale: true,
+					animateRotate: true
+				}
+			}
+		}
+		
+		var ctx = document.getElementById('pieReportPct').getContext('2d');
+		window.doughnutReport = new Chart(ctx, config);
+	}
+	
+	$scope.doughnutReportIndices = function() {
+		var config = {
+			type: 'doughnut',
+			data: {
+				datasets: [{
+					data: [
+						$filter('number')($scope.simulation.report.kappa),
+						$filter('number')($scope.simulation.report.errorrate)
+					],
+					backgroundColor: [
+						window.chartColors.blue,
+						window.chartColors.yellow
+					],
+					label: 'Dataset 1'
+				}],
+				labels: [
+					'Kappa: ',
+					'Error Rate: '
+				]
+			},
+			options: {
+				responsive: true,
+				legend: {
+					display: false,
+					position: 'top',
+				},
+				title: {
+					display: true,
+					text: 'Indices (Kappa e Erro Rate)'
+				},
+				animation: {
+					animateScale: true,
+					animateRotate: true
+				}
+			}
+		}
+		
+		var ctx = document.getElementById('doughnutReportIndices').getContext('2d');
+		window.doughnutReport = new Chart(ctx, config);
+	}
 	
 	$scope.simulations = function() {
 		$scope.application = {
@@ -90,7 +209,7 @@ angular.module("oracle-test").controller('StatisticController', [ '$scope', '$co
 				url: $stateParams.url
 		}
 		
-		StatisticService.getSimulations($scope.application.id).success(function(data) {
+		StatisticService.getSimulations($scope.application).success(function(data) {
 			if(data.length > 0) {
 				$scope.application.simulations = data;
 				$scope.application.qntSimulation = data.length;
@@ -102,4 +221,5 @@ angular.module("oracle-test").controller('StatisticController', [ '$scope', '$co
 			console.log("Deu errado ao listar as applicações");
 		});
 	}
+	
 }])
--- src/main/webapp/resources/js/services/StatisticService.js
+++ src/main/webapp/resources/js/services/StatisticService.js
@@ -1,21 +1,50 @@
 angular.module("oracle-test").factory("StatisticService", ["$http", "$q", "$cookieStore", function($http, $q, $cookieStore){
-	var result = null;
+	var statistics = null;
+	var application = null;
 	
+	/**
+	 * @author: José Reis R Santiago
+	 * @description: retorna as Statistics de uma aplicação.
+	 */
 	function getStatisticApplication(idApp) {
-		if(result == null) {
-			console.log("result: ", result);
-			result = $http.get("http://localhost:8080/oracle-test/backend/application/statistic/" + idApp);
+		if(statistics == null) {
+			statistics = $http.get("http://localhost:8080/oracle-test/backend/application/statistic/" + idApp);
 		}
 		
-		return result;
+		return statistics;
 	}
 	
-	function getSimulations(idApp) {
-		return $http.get("http://localhost:8080/oracle-test/backend/simulation/list/" + idApp);	
+	/**
+	 * @author: José Reis R Santiago
+	 * @description: 
+	 */
+	function getSimulationsApp(app) {
+		if(application==null || application.i != app.id) {
+			application = app;
+		}
+		
+		return $http.get("http://localhost:8080/oracle-test/backend/simulation/list/" + application.id);
+	}
+	
+	/**
+	 * @author: José Reis R Santiago
+	 * @description: busca e retorna uma simulação de uma aplicação pelo id
+	 */
+	function getSimulation(idSimulation) {
+		if(application != null) {
+			for(var i = 0; i < application.qntSimulation; i++) {
+				if(application.simulations[i].id == idSimulation) {
+					return application.simulations[i];
+				}
+			}
+		} else {
+			return null;
+		}
 	}
 	
 	return {
-		getSimulations: getSimulations,
+		getSimulation: getSimulation,
+		getSimulations: getSimulationsApp,
 		getStatisticApplication: getStatisticApplication
 	}
 }]);
